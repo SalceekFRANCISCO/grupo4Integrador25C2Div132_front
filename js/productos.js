e7e3776a5cc0eb631dc3074ca57be4e05956f959
Nasho <NachoFonta12@gmail.com>
2025-11-30 14:07:21 -0300

    Arreglos en imágenes, función para ordenar, cambios estilísticos

--- js/productos.js
+++ js/productos.js
@@ -1,29 +1,29 @@
 let contenedorProductos = document.getElementById("contenedor-productos");
-const url = "http://localhost:3000/api/productos"
+const url = "http://localhost:3000/api/productos";
+let productos = [];
 
 async function obtenerProductos() {
-    
     try {
         let respuesta = await fetch(url)
         let data = await respuesta.json();
 
-        console.log(contenedorProductos);
-        let productos = data.payload;
+        //console.log(contenedorProductos);
+        productos = data.payload;
         mostrarProductos(productos);
 
     } catch(error) {
-
         console.log(error);
 
     }
-                    
+
 }
 
 function mostrarProductos(array) {
-    console.table(array);
-    console.log(array)
-
+//    console.table(array);
+//    console.log(array)
     let htmlProducto = "";
+    console.log(array);
+//    ordenarProductos(array, 1);
 
     array.forEach(producto => {
         cantidad = mostrarCantidad(producto);
@@ -35,7 +35,7 @@ function mostrarProductos(array) {
             <p>${cantidad}</p>
         </div>`;
     });
-    console.log(htmlProducto);
+    //console.log(htmlProducto);
     contenedorProductos.innerHTML = htmlProducto;
 }
 
@@ -54,8 +54,50 @@ function mostrarCantidad(producto) {
     }
 }
 
+function ordenarProductos(array, parametro){
+    switch (parametro) {
+        case 1: {
+                for(let i = 0; i < array.length; i++) {
+                    for (let j = 0; j < i; j++) {
+                        if (array[i].nombre < array[j].nombre) {
+                            let temporal = array[j];
+                            array[j] = array[i];
+                            array[i] = temporal;
+                        }
+                    } 
+                }
+            console.log("Ordeno por nombre" + array);
+            break;
+        }
+        case -1: {
+                for(let i = 0; i < array.length; i++) {
+                    for (let j = 0; j < i; j++) {
+                        if (array[i].precio < array[j].precio) {
+                            let temporal = array[j];
+                            array[j] = array[i];
+                            array[i] = temporal;
+                        }
+                    } 
+                }
+            console.log("Ordeno por precio" + array);
+            break;
+        }
+    }
+    console.log(array);
+    mostrarProductos(array)
+}
+
+function configurarEventos() {
+    let ordenarSelect = document.getElementById("ordenar-select");
+    ordenarSelect.addEventListener('change', (event) => {
+        console.log(productos);
+        ordenarProductos(productos, parseInt(event.target.value));
+        });
+}
+
 function init() {
     obtenerProductos();
+    configurarEventos();
 }
 
 init();
