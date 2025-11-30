e5256f94b42ba074185d7e39f5a2d6f178f98fca
Francisco Salceek <salceekmunozfrancisco@gmail.com>
2025-11-30 02:29:41 -0300

    agregadas las img, arreglados mostrar productos, ahora se muestran correctamente y de paso arregle el css para dar detalles.

---
+++ js/productos.js
@@ -0,0 +1,45 @@
+let contenedorProductos = document.getElementById("contenedor-productos");
+const url = "http://localhost:3000/api/productos"
+
+async function obtenerProductos() {
+    
+    try {
+        let respuesta = await fetch(url)
+        let data = await respuesta.json();
+
+        console.log(contenedorProductos);
+        let productos = data.payload;
+        mostrarProductos(productos);
+
+    } catch(error) {
+
+        console.log(error);
+
+    }
+                    
+}
+
+function mostrarProductos(array) {
+    console.table(array);
+    console.log(array)
+
+    let htmlProducto = "";
+
+    array.forEach(producto => {
+        htmlProducto += `
+        <div class = "card-producto">
+            <img src="${producto.img_url}" alt="${producto.nombre}">
+            <h5>${producto.nombre}</h5>
+            <p> $${producto.precio}</p>
+        </div>`;
+        
+    });
+    console.log(htmlProducto);
+    contenedorProductos.innerHTML = htmlProducto;
+}
+
+function init() {
+    obtenerProductos();
+}
+
+init();
